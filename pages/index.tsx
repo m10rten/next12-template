20e5bd5847a79d1d11e101f3ad997d4939f3e1f2
morty <mr.vanderlei@student.han.nl>
2022-01-01 13:55:44 +0100

    next & tailwind & typescript & eslint airbnb

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => (
+  <>
+    <h2>title</h2>
+    <p>text</p>
+  </>
+);
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './pages/**/*.{ts,js,tsx,jsx}',
+    './components/**/*.{ts,js,tsx,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
